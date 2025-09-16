5c5a4bf95edb9cfdcc705dffbb285ca81d8caf66
rimsb <r.sboui@eloaded.eu>
2025-09-16 15:53:07 +0200

    fix formatting

--- client/src/components/SnackForm.tsx
+++ client/src/components/SnackForm.tsx
@@ -1,127 +1,154 @@
 import React from 'react';
-import {useCallback, useState } from 'react';
+import { useCallback, useState } from 'react';
 import 'react-notifications/lib/notifications.css';
 import { NotificationManager } from 'react-notifications';
 import { Snack } from '../models/snack';
 import Axios from 'axios';
 import Button from 'react-bootstrap/Button';
-import {Modal,Form,FormGroup, Row, Col} from 'react-bootstrap/';
+import { Modal, Form, FormGroup, Row, Col } from 'react-bootstrap/';
 import { getLocalDateTimeInput } from '../utils/utilsUI.tsx';
-import './SnackForm.scss'
+import './SnackForm.scss';
 import useSnackStore from '../stores/snackStore.tsx';
 
 //form props
-export type formProps= {
-  showForm: boolean,
-  callbackModal:any,
-  selectedSnack?:Snack
-}
+export type formProps = {
+  showForm: boolean;
+  callbackModal: any;
+  selectedSnack?: Snack;
+};
 /**
  * Create and edit form for a snack.
  * @param props
  * @returns createEditForm of a snack
  */
-function SnackForm(props:formProps) {
+function SnackForm(props: formProps) {
   //store states
-const addSnack = useSnackStore(state=>state.addSnack);
-const editSnack = useSnackStore(state=>state.editSnack);
+  const addSnack = useSnackStore((state) => state.addSnack);
+  const editSnack = useSnackStore((state) => state.editSnack);
 
-    //initial states in edit mode:
-    const initialValues:Partial<Snack> = props.selectedSnack?{
-      name: props.selectedSnack.name,
-      isFavorite: props.selectedSnack.isFavorite,
-      lastDayConsumed: props.selectedSnack.lastDayConsumed,
-      calories:{
-        value: props.selectedSnack.calories?.value,
-        unit: props.selectedSnack.calories?.unit
+  //initial states in edit mode:
+  const initialValues: Partial<Snack> = props.selectedSnack
+    ? {
+        name: props.selectedSnack.name,
+        isFavorite: props.selectedSnack.isFavorite,
+        lastDayConsumed: props.selectedSnack.lastDayConsumed,
+        calories: {
+          value: props.selectedSnack.calories?.value,
+          unit: props.selectedSnack.calories?.unit,
+        },
       }
-       
-    }:{
-      name:'',
-      isFavorite: false,
-      lastDayConsumed: new Date(),
-      calories: {
-        value: 0,
-        unit: 'Kcal'
-      }
-    }
+    : {
+        name: '',
+        isFavorite: false,
+        lastDayConsumed: new Date(),
+        calories: {
+          value: 0,
+          unit: 'Kcal',
+        },
+      };
   //local states definition
-  const [name,setName] = useState(initialValues.name)
-  const [favorite,setFavorite] = useState(initialValues.isFavorite);
-  const [lastDay,setLastDay] = useState(initialValues.lastDayConsumed);
-  const [caloriesValue,setCaloriesValue] = useState(initialValues?.calories?.value);
-  const [cloriesUnit,setCaloriesUnit] = useState(initialValues?.calories?.unit);
+  const [name, setName] = useState(initialValues.name);
+  const [favorite, setFavorite] = useState(initialValues.isFavorite);
+  const [lastDay, setLastDay] = useState(initialValues.lastDayConsumed);
+  const [caloriesValue, setCaloriesValue] = useState(
+    initialValues?.calories?.value
+  );
+  const [cloriesUnit, setCaloriesUnit] = useState(
+    initialValues?.calories?.unit
+  );
 
-//callbacks
-  const createSnack = useCallback(()=>{
-    let snackToAdd:Partial<Snack>= {
-      name:name,
-      lastDayConsumed:lastDay,
-      isFavorite:favorite,
-      calories:{
-        value:caloriesValue as number,
-        unit:cloriesUnit as string
-      }
-    }
-    console.log("snack to be created: "+snackToAdd.calories)
+  //callbacks
+  const createSnack = useCallback(() => {
+    let snackToAdd: Partial<Snack> = {
+      name: name,
+      lastDayConsumed: lastDay,
+      isFavorite: favorite,
+      calories: {
+        value: caloriesValue as number,
+        unit: cloriesUnit as string,
+      },
+    };
+    console.log('snack to be created: ' + snackToAdd.calories);
     Axios.post('http://localhost:3001/create-snack', snackToAdd)
-    .then((response)=>{
-      NotificationManager.success('The snack has been added', 'Success', 2000);
-    })
-    .catch((error)=>{
-      NotificationManager.error('The snack was not added, something went wrong', 'Failure', 5000)
-    })
-    
-   },[caloriesValue, cloriesUnit, favorite, lastDay, name])
+      .then((response) => {
+        NotificationManager.success(
+          'The snack has been added',
+          'Success',
+          2000
+        );
+      })
+      .catch((error) => {
+        NotificationManager.error(
+          'The snack was not added, something went wrong',
+          'Failure',
+          5000
+        );
+      });
+  }, [caloriesValue, cloriesUnit, favorite, lastDay, name]);
 
-const updateSnack = useCallback(()=> {
-  if(!!props.selectedSnack) {
-    let snack:Snack= {
-      _id:props.selectedSnack._id,
-      name:name as string,
-      lastDayConsumed:lastDay as Date,
-      isFavorite:favorite as boolean,
-      calories:{
-        value:caloriesValue as number,
-        unit:cloriesUnit as string
-      }
+  const updateSnack = useCallback(() => {
+    if (!!props.selectedSnack) {
+      let snack: Snack = {
+        _id: props.selectedSnack._id,
+        name: name as string,
+        lastDayConsumed: lastDay as Date,
+        isFavorite: favorite as boolean,
+        calories: {
+          value: caloriesValue as number,
+          unit: cloriesUnit as string,
+        },
+      };
+      Axios.put('http://localhost:3001/snack/' + snack._id, snack)
+        .then((response) => {
+          NotificationManager.success(
+            'The snack has been updated',
+            'Success',
+            2000
+          );
+        })
+        .catch((error) => {
+          NotificationManager.error(
+            'The snack was not updated, something went wrong',
+            'Failure',
+            5000
+          );
+        });
+    } else {
+      console.warn(' No snack found to edit here');
     }
-    Axios.put('http://localhost:3001/snack/'+ snack._id, snack)
-    .then((response)=>{
-      NotificationManager.success('The snack has been updated', 'Success', 2000);
+  }, [
+    caloriesValue,
+    cloriesUnit,
+    favorite,
+    lastDay,
+    name,
+    props.selectedSnack,
+  ]);
 
-    })
-    .catch((error)=>{
-      NotificationManager.error('The snack was not updated, something went wrong', 'Failure', 5000)
-    })
-  } else{
-    console.warn(" No snack found to edit here");
-  }
-},[caloriesValue,
-  cloriesUnit,
-  favorite,
-  lastDay,
-  name,
-  props.selectedSnack])
-
-const CreateUpdateSnack = useCallback(()=>{
-  if(props?.selectedSnack?._id) {
-    updateSnack()
-  } else {
-    createSnack()
-  }
-  props.callbackModal()
-},[createSnack, props, updateSnack])
+  const CreateUpdateSnack = useCallback(() => {
+    if (props?.selectedSnack?._id) {
+      updateSnack();
+    } else {
+      createSnack();
+    }
+    props.callbackModal();
+  }, [createSnack, props, updateSnack]);
 
   //UI
-return(
-  <Modal show={props.showForm} onHide={props.callbackModal}>
-        <Modal.Header closeButton>
-          <Modal.Title>{props.selectedSnack?._id?<label>Update snack</label>:<label>Add new snack</label>}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <>
-          <Form className='create-form'>
+  return (
+    <Modal show={props.showForm} onHide={props.callbackModal}>
+      <Modal.Header closeButton>
+        <Modal.Title>
+          {props.selectedSnack?._id ? (
+            <label>Update snack</label>
+          ) : (
+            <label>Add new snack</label>
+          )}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <>
+          <Form className="create-form">
             <Form.Group className="mb-3">
               <Form.Label>Name</Form.Label>
               <Form.Control
@@ -130,39 +157,49 @@ return(
                 placeholder="snack name"
                 value={name}
                 pattern="[a-zA-Z]"
-                onChange={(event)=>setName(event.target.value)}
+                onChange={(event) => setName(event.target.value)}
                 autoFocus
               />
             </Form.Group>
-            <Form.Group
-              className="mb-3"
-            >
+            <Form.Group className="mb-3">
               <Form.Label>Last Date consumed:</Form.Label>
               <Form.Control
-              type="datetime-local"
-              value={getLocalDateTimeInput(lastDay)}
-              max={getLocalDateTimeInput(new Date())}
-              onChange={(event)=>{if (event.target.value !==null){setLastDay(new Date(event.target.value))} else {setLastDay(new Date())}}}
+                type="datetime-local"
+                value={getLocalDateTimeInput(lastDay)}
+                max={getLocalDateTimeInput(new Date())}
+                onChange={(event) => {
+                  if (event.target.value !== null) {
+                    setLastDay(new Date(event.target.value));
+                  } else {
+                    setLastDay(new Date());
+                  }
+                }}
               />
             </Form.Group>
             <FormGroup className="mb-3">
-            <Form.Label>Calories: </Form.Label>
+              <Form.Label>Calories: </Form.Label>
               <Row className="calories-input">
                 <Col>
                   <Form.Control
-                  aria-label="Calories"
-                  type="number"
-                  placeholder="calories"
-                  value={caloriesValue}
-                  min='0'
-                  onChange={(event)=>setCaloriesValue(parseInt(event.target.value))}/>
+                    aria-label="Calories"
+                    type="number"
+                    placeholder="calories"
+                    value={caloriesValue}
+                    min="0"
+                    onChange={(event) =>
+                      setCaloriesValue(parseInt(event.target.value))
+                    }
+                  />
                 </Col>
                 <Col>
-                  <Form.Select aria-label="Unit"
-                  onChange={(event)=>{
-                    setCaloriesUnit(event.target.value)}}>
-                  <option value="Kcal">Kcal</option>
-                  <option value="Kj">Kj</option>
+                  <Form.Select
+                    aria-label="Unit"
+                    onChange={(event) => {
+                      setCaloriesUnit(event.target.value);
+                    }}
+                  >
+                    <option value="Kcal">Kcal</option>
+                    <option value="Kj">Kj</option>
                   </Form.Select>
                 </Col>
               </Row>
@@ -172,21 +209,23 @@ return(
                 type="checkbox"
                 label="Favorite"
                 checked={favorite}
-                onChange={(event)=>{setFavorite(event.target.checked)}}
+                onChange={(event) => {
+                  setFavorite(event.target.checked);
+                }}
               />
             </Form.Group>
           </Form>
-          </>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="primary" onClick={CreateUpdateSnack}>
-            Save
-          </Button>
-          <Button variant="secondary" onClick={props.callbackModal}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-);
+        </>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="primary" onClick={CreateUpdateSnack}>
+          Save
+        </Button>
+        <Button variant="secondary" onClick={props.callbackModal}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
 }
-export default SnackForm
+export default SnackForm;
--- client/src/components/SnackList.tsx
+++ client/src/components/SnackList.tsx
@@ -1,6 +1,15 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import Axios from 'axios';
-import { Card, Container, Row, Col, Button, Modal, Form, InputGroup} from 'react-bootstrap/';
+import {
+  Card,
+  Container,
+  Row,
+  Col,
+  Button,
+  Modal,
+  Form,
+  InputGroup,
+} from 'react-bootstrap/';
 import { Snack } from '../models/snack';
 import { NotificationManager } from 'react-notifications';
 import './SnackList.scss';
@@ -8,141 +17,199 @@ import SnackForm from './SnackForm.tsx';
 import { formatDate, getFavoriteIconStatus } from '../utils/utilsUI.tsx';
 import useSnackStore from '../stores/snackStore.tsx';
 
-
 /**
- * All the component's functions and login related to the all displayed snack List. 
+ * All the component's functions and login related to the all displayed snack List.
  * @returns the snacklist component
  */
 function SnackList() {
   // local state
-const snacks = useSnackStore(state=>state.snacks);
-const setSnacks = useSnackStore(state=>state.setSnacks)
-const [show, setShow]= useState(false);
-const [showForm,setShowForm] = useState(false);
-const[selectedSnack,setSelectedSnack]=useState(():Snack=>{return {} as Snack});
-const [searchName,setSearchName]=useState('');
+  const snacks = useSnackStore((state) => state.snacks);
+  const setSnacks = useSnackStore((state) => state.setSnacks);
+  const [show, setShow] = useState(false);
+  const [showForm, setShowForm] = useState(false);
+  const [selectedSnack, setSelectedSnack] = useState((): Snack => {
+    return {} as Snack;
+  });
+  const [searchName, setSearchName] = useState('');
 
+  // open create form handler
+  const handleCreateForm = useCallback(() => {
+    console.log('show the create form');
+    setShowForm(true);
+  }, []);
+  // open form to edit the selected snack
+  const openCreateEditSnack = useCallback((snackToUpdate: Snack) => {
+    console.log('the update callback to open the form');
+    setSelectedSnack(snackToUpdate);
+    setShowForm(true);
+  }, []);
 
-// open create form handler
-const handleCreateForm = useCallback(()=>{
-  console.log("show the create form")
-  setShowForm(true)
-},[])
-// open form to edit the selected snack
-const openCreateEditSnack= useCallback((snackToUpdate:Snack)=> {
-  console.log("the update callback to open the form")
-  setSelectedSnack(snackToUpdate);
-  setShowForm(true);
-  },[])
+  // callback modal for closing edit form:
+  const callbackModal = useCallback(() => {
+    setShowForm(false);
+  }, []);
 
-// callback modal for closing edit form:
-const callbackModal = useCallback(()=>{
-  setShowForm(false)
-},[])
-
-// open deletion confirmation modal
-const openConfirmDeleteModal= useCallback((snackToDelete:Snack)=> {
-  setSelectedSnack(snackToDelete);
-  setShow(true);
-},[setShow])
-//close delete confirmation modal
-const handleClose = useCallback(()=>{
-  setShow(false);
-  },[setShow])
-  //deletion Handler
-const handleDeletion = useCallback((snackID:string)=> {
-  Axios.delete('http://localhost:3001/delete-snack/'+ snackID).then((response)=>{
-  setShow(false);
-  NotificationManager.success(response.data +' has been removed','Success',2000)
-  }).catch(error => {
-    console.log('something went wrong', error)
-    NotificationManager.success('Deletion process idd not went through','Failure',2000)
-  })
-},[])
-// deletion confirmation modal
-const ConfirmationModal = () =>{
-  return (
-    <Modal show={show} onHide={handleClose}>
-      <Modal.Header closeButton>
-        <Modal.Title>Confirmation Required</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>You are about to delete this snack: {selectedSnack.name}!</Modal.Body>
-      <Modal.Footer>
-      <Button variant="primary" onClick={()=>handleDeletion(selectedSnack._id)}>
-          Delete
-        </Button>
-        <Button variant="secondary" onClick={handleClose}>
-          Close
-        </Button>
-      </Modal.Footer>
-    </Modal>
+  // open deletion confirmation modal
+  const openConfirmDeleteModal = useCallback(
+    (snackToDelete: Snack) => {
+      setSelectedSnack(snackToDelete);
+      setShow(true);
+    },
+    [setShow]
   );
-}
+  //close delete confirmation modal
+  const handleClose = useCallback(() => {
+    setShow(false);
+  }, [setShow]);
+  //deletion Handler
+  const handleDeletion = useCallback((snackID: string) => {
+    Axios.delete('http://localhost:3001/delete-snack/' + snackID)
+      .then((response) => {
+        setShow(false);
+        NotificationManager.success(
+          response.data + ' has been removed',
+          'Success',
+          2000
+        );
+      })
+      .catch((error) => {
+        console.log('something went wrong', error);
+        NotificationManager.success(
+          'Deletion process idd not went through',
+          'Failure',
+          2000
+        );
+      });
+  }, []);
+  // deletion confirmation modal
+  const ConfirmationModal = () => {
+    return (
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Confirmation Required</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          You are about to delete this snack: {selectedSnack.name}!
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="primary"
+            onClick={() => handleDeletion(selectedSnack._id)}
+          >
+            Delete
+          </Button>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  };
 
+  // callback to children to update the snack list:
+  const updateSnackList = useCallback(() => {
+    Axios.get('http://localhost:3001/snacks').then((res) =>
+      setSnacks(res.data)
+    );
+  }, [setSnacks]);
+  // get the snacks after refreshing
+  useEffect(() => {
+    updateSnackList();
+  }, [updateSnackList]);
 
-// callback to children to update the snack list:
-const updateSnackList= useCallback(()=>{
-  Axios.get('http://localhost:3001/snacks').then((res)=>setSnacks(res.data))
-},[setSnacks])
-// get the snacks after refreshing
-useEffect(()=> {
-  updateSnackList()
-},[updateSnackList])
-
-  return(
-    <Container className='snacks-container'>
+  return (
+    <Container className="snacks-container">
       <Row>
         <Form>
-          <InputGroup className='my-3'>
-            <Form.Control onChange={(e)=>setSearchName(e.target.value)} placeholder='Search for snacks by names'>
-            </Form.Control>
+          <InputGroup className="my-3">
+            <Form.Control
+              onChange={(e) => setSearchName(e.target.value)}
+              placeholder="Search for snacks by names"
+            ></Form.Control>
           </InputGroup>
-         </Form>
+        </Form>
       </Row>
       <Row>
         <Col>
-          <Card style={{ width: '18rem' }} bg="light" onClick={handleCreateForm} className='add-card'>
+          <Card
+            style={{ width: '18rem' }}
+            bg="light"
+            onClick={handleCreateForm}
+            className="add-card"
+          >
             <Card.Body>
-              <svg xmlns="http://www.w3.org/2000/svg" width="9rem" fill="grey" className="bi bi-file-plus" viewBox="1 0 13 13">
-              <path d="M8.5 6a.5.5 0 0 0-1 0v1.5H6a.5.5 0 0 0 0 1h1.5V10a.5.5 0 0 0 1 0V8.5H10a.5.5 0 0 0 0-1H8.5V6z"/>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="9rem"
+                fill="grey"
+                className="bi bi-file-plus"
+                viewBox="1 0 13 13"
+              >
+                <path d="M8.5 6a.5.5 0 0 0-1 0v1.5H6a.5.5 0 0 0 0 1h1.5V10a.5.5 0 0 0 1 0V8.5H10a.5.5 0 0 0 0-1H8.5V6z" />
               </svg>
             </Card.Body>
           </Card>
         </Col>
 
-      {snacks.filter((snack:Snack) => {
-        return searchName.toLowerCase()!==''?snack?.name?.toLowerCase().includes(searchName.toLowerCase()):snack
-      }).map((snack:Snack,key)=>
-        <Col key={key}>
-          <Card key={snack._id} style={{ width: '18rem' }} className="snack-item">
-          {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
-            <Card.Body>
-              <Card.Title>{snack.name}</Card.Title>
-              <Card.Text className="card-text">
-               <>
-                <label>Last Day Consumed: </label> {formatDate(snack?.lastDayConsumed)}
-                <br/>
-                <label>Favorite: </label> {getFavoriteIconStatus(snack?.isFavorite)}
-                <br/>
-                <label>Calories: </label> {snack?.calories?.value} {snack?.calories?.unit}
-                </>
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <Button variant="primary" onClick={(event)=>openCreateEditSnack(snack)}>Update</Button>
-              <Button variant="secondary" onClick={(event)=>openConfirmDeleteModal(snack)}>Remove</Button>
-            </Card.Footer>
-          </Card>
-        </Col>
-      )}
+        {snacks
+          .filter((snack: Snack) => {
+            return searchName.toLowerCase() !== ''
+              ? snack?.name?.toLowerCase().includes(searchName.toLowerCase())
+              : snack;
+          })
+          .map((snack: Snack, key) => (
+            <Col key={key}>
+              <Card
+                key={snack._id}
+                style={{ width: '18rem' }}
+                className="snack-item"
+              >
+                {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+                <Card.Body>
+                  <Card.Title>{snack.name}</Card.Title>
+                  <Card.Text className="card-text">
+                    <>
+                      <label>Last Day Consumed: </label>{' '}
+                      {formatDate(snack?.lastDayConsumed)}
+                      <br />
+                      <label>Favorite: </label>{' '}
+                      {getFavoriteIconStatus(snack?.isFavorite)}
+                      <br />
+                      <label>Calories: </label> {snack?.calories?.value}{' '}
+                      {snack?.calories?.unit}
+                    </>
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <Button
+                    variant="primary"
+                    onClick={(event) => openCreateEditSnack(snack)}
+                  >
+                    Update
+                  </Button>
+                  <Button
+                    variant="secondary"
+                    onClick={(event) => openConfirmDeleteModal(snack)}
+                  >
+                    Remove
+                  </Button>
+                </Card.Footer>
+              </Card>
+            </Col>
+          ))}
       </Row>
-      {snacks.length===0 &&
-      <div>
-        <label className='no-data-label'>-No data has been found-</label>
-      </div>}
-      <ConfirmationModal/>
-      <SnackForm showForm={showForm} selectedSnack={selectedSnack} callbackModal={callbackModal}/>
+      {snacks.length === 0 && (
+        <div>
+          <label className="no-data-label">-No data has been found-</label>
+        </div>
+      )}
+      <ConfirmationModal />
+      <SnackForm
+        showForm={showForm}
+        selectedSnack={selectedSnack}
+        callbackModal={callbackModal}
+      />
     </Container>
   );
 }
-export default SnackList
+export default SnackList;
